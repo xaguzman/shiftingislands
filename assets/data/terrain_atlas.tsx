be3fae1bb16e2c2eb4cb5c0d5d803ff12fc91d2e
Xavier Guzman <arkalain@gmail.com>
2014-01-27 21:59:47 -0600

    First pages commit

---
+++ assets/data/terrain_atlas.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terrain_atlas" tilewidth="32" tileheight="32">
+ <image source="terrain_atlas.png" width="1024" height="1024"/>
+ <tile id="118">
+  <properties>
+   <property name="type" value="path"/>
+  </properties>
+ </tile>
+ <tile id="266">
+  <properties>
+   <property name="type" value="respawn"/>
+  </properties>
+ </tile>
+ <tile id="267">
+  <properties>
+   <property name="type" value="activeRespawn"/>
+  </properties>
+ </tile>
+</tileset>
